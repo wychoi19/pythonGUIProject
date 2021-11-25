5dafe7303ddedf98f77997f0a6b9b47993bd1461
WooYoungChoi <wychoi19@yonsei.ac.kr>
2021-11-25 18:31:31 +0900

    init(11.25)

---
+++ addImage.py
@@ -0,0 +1,8 @@
+import tkinter as t
+
+root = t.Tk()
+root.title("[파이썬] 이미지 합치기 프로그램")
+root.geometry("640x480+100+300") #창 크기 지정 & 위치 지정
+root.resizable(False, False) #창 크기 조절 불가능
+
+root.mainloop() #창이 꺼지지 않게 loop가 돈다.
